f765c0749b4bcc174b300db3bba8b4bb80e513f9
Juan M. Martinez <bloodamn@gmail.com>
2020-08-08 19:47:26 -0500

    flex implement

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,8 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { IntroComponent } from './pages/intro/intro.component';
-
+import { CoreModule } from './core/core.module';
+import { FlexLayoutModule } from '@angular/flex-layout';
 @NgModule({
   declarations: [
     AppComponent,
@@ -12,7 +13,9 @@ import { IntroComponent } from './pages/intro/intro.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CoreModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
